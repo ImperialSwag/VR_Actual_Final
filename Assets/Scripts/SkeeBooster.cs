03131f6e0081e2190f75545da6594789da01203d
Efe Ayvaz <efeayvaz28@gmail.com>
2021-06-15 19:27:03 +0300

    skeeball

---
+++ Assets/Scripts/SkeeBooster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeeBooster : MonoBehaviour
+{
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Basketball"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * -10, ForceMode.Acceleration);
+        }
+    }
+
+}
